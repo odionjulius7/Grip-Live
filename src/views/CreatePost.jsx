987a419f5a57597a8a0a8eb10ce92951faee5987
Odion Julius <odionjulius7@gmail.com>
2024-07-18 05:43:02 +0100

    build

--- src/views/CreatePost.jsx
+++ src/views/CreatePost.jsx
@@ -7,7 +7,14 @@ import {
   Select,
   MenuItem,
   TextField,
+  Grid,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
 } from "@mui/material";
+import { Checkbox } from "@mui/material";
+import { Button } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { getCategories } from "../features/category/categorySlice";
 import { toast } from "react-toastify";
@@ -134,256 +141,252 @@ function CreatePost() {
 
   return (
     <div className="container-fluid mt-1 mb-4 px-5">
-      <h1 className="mb-4">Create Post</h1>
       <div className="row m-3 px-5">
+        <h1 className="mb-4">Create Post</h1>
         <form onSubmit={formik.handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="file" className="form-label">
-              File (video, audio, or any)
-            </label>
-            <input
-              id="file"
-              name="file"
-              type="file"
-              className="form-control"
-              onChange={handleFileChange}
-            />
-            {formik.errors.file && (
-              <div className="text-danger">{formik.errors.file}</div>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="image" className="form-label">
-              Thumbnail
-            </label>
-            <input
-              id="image"
-              name="image"
-              type="file"
-              accept="image/*"
-              className="form-control"
-              onChange={handleImageChange}
-            />
-            {formik.errors.image && (
-              <div className="text-danger">{formik.errors.image}</div>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="file_type" className="form-label">
-              File Type
-            </label>
-            <select
-              id="file_type"
-              name="file_type"
-              className="form-control"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.file_type}
-            >
-              <option value="">Select a file type</option>
-              <option value="video">Video</option>
-              <option value="audio">Audio</option>
-              <option value="image">Image</option>
-            </select>
-            {formik.touched.file_type && formik.errors.file_type && (
-              <div className="text-danger">{formik.errors.file_type}</div>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="title" className="form-label">
-              Title
-            </label>
-            <input
-              id="title"
-              name="title"
-              type="text"
-              className="form-control"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.title}
-            />
-            {formik.touched.title && formik.errors.title && (
-              <div className="text-danger">{formik.errors.title}</div>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <FormControl fullWidth>
-              <InputLabel id="category-label">Category</InputLabel>
-              <Select
-                id="category"
-                name="category"
-                multiple
-                value={selectedCategories}
-                onChange={handleChange}
-                onBlur={formik.handleBlur}
-                inputProps={{
-                  name: "category",
-                  id: "category",
-                }}
-                renderValue={(selected) => selected.join(", ")}
-              >
-                {categoryState?.data?.map((item, index) => (
-                  <MenuItem key={index} value={item?.name}>
-                    {item?.name}
-                  </MenuItem>
-                ))}
-              </Select>
-              {formik.touched.category && formik.errors.category && (
-                <div className="text-danger">{formik.errors.category}</div>
+          <Grid container spacing={3} className="form-grid">
+            <Grid item xs={12} sm={6}>
+              <label htmlFor="file" className="form-label">
+                File (video, audio, or image)
+              </label>
+              <input
+                id="file"
+                name="file"
+                type="file"
+                className="form-control"
+                onChange={handleFileChange}
+              />
+              {formik.errors.file && (
+                <div className="text-danger">{formik.errors.file}</div>
               )}
-            </FormControl>
-          </div>
+            </Grid>
 
-          <div className="mb-3">
-            <label htmlFor="bible_book" className="form-label">
-              Bible Book
-            </label>
-            <input
-              id="bible_book"
-              name="bible_book"
-              type="text"
-              className="form-control"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.bible_book}
-            />
-            {formik.touched.bible_book && formik.errors.bible_book && (
-              <div className="text-danger">{formik.errors.bible_book}</div>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="bible_chapter" className="form-label">
-              Bible Chapter
-            </label>
-            <input
-              id="bible_chapter"
-              name="bible_chapter"
-              type="number"
-              className="form-control"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.bible_chapter}
-            />
-            {formik.touched.bible_chapter && formik.errors.bible_chapter && (
-              <div className="text-danger">{formik.errors.bible_chapter}</div>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="bible_verse" className="form-label">
-              Bible Verse
-            </label>
-            <input
-              id="bible_verse"
-              name="bible_verse"
-              type="number"
-              className="form-control"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.bible_verse}
-            />
-            {formik.touched.bible_verse && formik.errors.bible_verse && (
-              <div className="text-danger">{formik.errors.bible_verse}</div>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="published_at" className="form-label">
-              Published At
-            </label>
-            <input
-              id="published_at"
-              name="published_at"
-              type="date"
-              className="form-control"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.published_at}
-            />
-            {formik.touched.published_at && formik.errors.published_at && (
-              <div className="text-danger">{formik.errors.published_at}</div>
-            )}
-          </div>
-
-          <div className="mb-3">
-            <label htmlFor="description" className="form-label">
-              Description
-            </label>
-            <textarea
-              id="description"
-              name="description"
-              className="form-control"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.description}
-            />
-            {formik.touched.description && formik.errors.description && (
-              <div className="text-danger">{formik.errors.description}</div>
-            )}
-          </div>
-
-          {/* <div className="mb-3 form-check">
-            <input
-              id="is_scheduled"
-              name="is_scheduled"
-              type="checkbox"
-              className="form-check-input"
-              onChange={formik.handleChange}
-              // onBlur={formik.handleBlur}
-              // checked={formik.values.is_scheduled}
-            />
-            <label htmlFor="is_scheduled" className="form-check-label">
-              Is Scheduled
-            </label>
-            {formik.touched.is_scheduled && formik.errors.is_scheduled && (
-              <div className="text-danger">{formik.errors.is_scheduled}</div>
-            )}
-          </div> */}
-          <div className="mb-3">
-            <label className="form-label">Is Scheduled</label>
-            <div className="form-check">
+            <Grid item xs={12} sm={6}>
+              <label htmlFor="image" className="form-label">
+                Thumbnail
+              </label>
               <input
-                id="isScheduledYes"
-                name="is_scheduled"
-                type="radio"
-                className="form-check-input"
-                value="true"
+                id="image"
+                name="image"
+                type="file"
+                accept="image/*"
+                className="form-control"
+                onChange={handleImageChange}
+              />
+              {formik.errors.image && (
+                <div className="text-danger">{formik.errors.image}</div>
+              )}
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <label htmlFor="file_type" className="form-label">
+                File Type
+              </label>
+              <select
+                id="file_type"
+                name="file_type"
+                className="form-control"
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
-                checked={formik.values.is_scheduled === true}
-              />
-              <label htmlFor="isScheduledYes" className="form-check-label">
-                Yes
+                value={formik.values.file_type}
+              >
+                <option value="">Select a file type</option>
+                <option value="video">Video</option>
+                <option value="audio">Audio</option>
+                <option value="image">Image</option>
+              </select>
+              {formik.touched.file_type && formik.errors.file_type && (
+                <div className="text-danger">{formik.errors.file_type}</div>
+              )}
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <label htmlFor="title" className="form-label">
+                Title
               </label>
-            </div>
-            <div className="form-check">
               <input
-                id="isScheduledNo"
-                name="is_scheduled"
-                type="radio"
-                className="form-check-input"
-                value="false"
+                id="title"
+                name="title"
+                type="text"
+                className="form-control"
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
-                checked={formik.values.is_scheduled === false}
+                value={formik.values.title}
               />
-              <label htmlFor="isScheduledNo" className="form-check-label">
-                No
-              </label>
-            </div>
-            {formik.touched.is_scheduled && formik.errors.is_scheduled && (
-              <div className="text-danger">{formik.errors.is_scheduled}</div>
-            )}
-          </div>
+              {formik.touched.title && formik.errors.title && (
+                <div className="text-danger">{formik.errors.title}</div>
+              )}
+            </Grid>
 
-          <button type="submit" className="btn btn-primary">
-            {isLoading ? "Sending" : "Send"}
-          </button>
+            <Grid item xs={12} sm={6}>
+              <label htmlFor="bible_book" className="form-label">
+                Bible Book
+              </label>
+              <input
+                id="bible_book"
+                name="bible_book"
+                type="text"
+                className="form-control"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.bible_book}
+              />
+              {formik.touched.bible_book && formik.errors.bible_book && (
+                <div className="text-danger">{formik.errors.bible_book}</div>
+              )}
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <label htmlFor="bible_chapter" className="form-label">
+                Bible Chapter
+              </label>
+              <input
+                id="bible_chapter"
+                name="bible_chapter"
+                type="number"
+                className="form-control"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.bible_chapter}
+              />
+              {formik.touched.bible_chapter && formik.errors.bible_chapter && (
+                <div className="text-danger">{formik.errors.bible_chapter}</div>
+              )}
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <label htmlFor="bible_verse" className="form-label">
+                Bible Verse
+              </label>
+              <input
+                id="bible_verse"
+                name="bible_verse"
+                type="number"
+                className="form-control"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.bible_verse}
+              />
+              {formik.touched.bible_verse && formik.errors.bible_verse && (
+                <div className="text-danger">{formik.errors.bible_verse}</div>
+              )}
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <label htmlFor="#" className="form-label">
+                Category
+              </label>
+              {/* <InputLabel id="category-label">Category</InputLabel> */}
+              <FormControl fullWidth>
+                <Select
+                  id="category"
+                  name="category"
+                  multiple
+                  value={selectedCategories}
+                  onChange={handleChange}
+                  onBlur={formik.handleBlur}
+                  inputProps={{
+                    name: "category",
+                    id: "category",
+                  }}
+                  renderValue={(selected) => selected.join(", ")}
+                >
+                  {categoryState?.data?.map((item, index) => (
+                    <MenuItem key={index} value={item?.name}>
+                      {item?.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+                {formik.touched.category && formik.errors.category && (
+                  <div className="text-danger">{formik.errors.category}</div>
+                )}
+              </FormControl>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              {formik.values.is_scheduled && (
+                <>
+                  {" "}
+                  <label htmlFor="published_at" className="form-label">
+                    Published At
+                  </label>
+                  <input
+                    id="published_at"
+                    name="published_at"
+                    type="datetime-local"
+                    className="form-control"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.published_at}
+                  />
+                </>
+              )}
+              {formik.touched.published_at && formik.errors.published_at && (
+                <div className="text-danger">{formik.errors.published_at}</div>
+              )}
+            </Grid>
+            {""}
+
+            {/* <Grid item xs={12} sm={6}>
+              <label htmlFor="description" className="form-label">
+                Description
+              </label>
+              <textarea
+                id="description"
+                name="description"
+                className="form-control"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.description}
+              />
+              {formik.touched.description && formik.errors.description && (
+                <div className="text-danger">{formik.errors.description}</div>
+              )}
+            </Grid> */}
+
+            <Grid item xs={12} sm={6}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    id="is_scheduled"
+                    name="is_scheduled"
+                    checked={formik.values.is_scheduled}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                  />
+                }
+                label="Is Scheduled"
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={12}>
+              <label htmlFor="description" className="form-label">
+                Description
+              </label>
+              <textarea
+                id="description"
+                name="description"
+                style={{
+                  height: "250px",
+                }}
+                className="form-control"
+                rows={17}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.description}
+              />
+            </Grid>
+
+            <Grid item xs={12} className="mt-4">
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                disabled={formik.isSubmitting}
+              >
+                Create Post
+              </Button>
+            </Grid>
+          </Grid>
         </form>
       </div>
     </div>
